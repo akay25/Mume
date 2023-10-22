ac707e605fff78b8fd65048873d327c802e113a5
Ajay M <ajay@shivy.co.in>
2023-10-22 17:08:55 +0530

    Code refactored

--- frontend/src/layout/AppLayout.vue
+++ frontend/src/layout/AppLayout.vue
@@ -60,16 +60,12 @@ const isOutsideClicked = (event) => {
 <template>
   <div class="layout-wrapper" :class="containerClass">
     <app-topbar></app-topbar>
-    <!-- <div class="layout-sidebar">
-      <app-sidebar></app-sidebar>
-    </div> -->
     <div class="layout-main-container">
       <div class="layout-main">
         <router-view></router-view>
       </div>
       <app-footer></app-footer>
     </div>
-    <!-- <app-config></app-config> -->
     <div class="layout-mask"></div>
   </div>
 </template>
