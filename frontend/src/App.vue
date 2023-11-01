11f8644bc9157ff241c94ef9fe5214ecac486674
Ajay M <ajay@shivy.co.in>
2023-11-01 19:46:56 +0530

    Added pinia state management

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,14 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue';
+import { useApplicationStore } from '@/stores/application';
+
+const applicationStore = useApplicationStore();
+
+onMounted(async () => {
+  await applicationStore.fetchLibaryPath();
+  console.log("woprking ", applicationStore.libraryPath);
+})
+</script>
 
 <template>
   <router-view />
---
+++ frontend/src/stores/application.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia';
+import { GetLibraryPath, SetLibraryPath } from '@wailsjs';;
+
+export const useApplicationStore = defineStore('application', {
+  state: () => ({
+    platform: "linux",
+    libraryPath: "",
+  }),
+  actions: {
+    async fetchLibaryPath() {
+      this.libraryPath = await GetLibraryPath();
+    }
+  }
+});
